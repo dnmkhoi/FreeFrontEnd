572e2576eaea32190f1703abfcbc239f8c911e70
chickenbadct10 <realmadridfc1995@gmail.com>
2020-05-02 19:38:22 +0700

    chia giao diện frontend

---
+++ resources/views/frontend/layout/partials/footer.blade.php
@@ -0,0 +1,68 @@
+<div class="footerContentWrapper">
+    <section id="footer">
+        <div class="footerContent">
+            <div class="col">
+                <ul>
+                    <li class="title alpha">Information</li>
+                    <li><a id="footerMenu_terms" href="/information#terms">Conditions générales</a></li>
+                </ul>
+            </div>
+
+            <div class="col">
+                <ul>
+                    <li class="title alpha">Travailler avec nous</li>
+                    <li><a id="footerMenu_partner" href="/partners/cpp">Partenaires de contenu</a></li>
+                </ul>
+            </div>
+
+            <div class="col">
+                <ul>
+                    <li class="title alpha">Assistance et aide</li>
+                    <li><a id="footerMenu_faq" href="/information#faq">FAQ</a></li>
+                </ul>
+            </div>
+
+            <div class="col">
+                <ul>
+                    <li class="title alpha">Découvrez</li>
+                    <li><a href="/blog">Blog Pornhub</a></li>
+                </ul>
+            </div>
+        </div>
+
+
+        <div class="languageWrapper js_dropdownBound" data-dropdown-id="2">
+            <div class="languagesLable">Language:</div>
+            <div class="languageSelected dropdownTrigger">
+                <div class="iconSphere"></div>
+                Français <div class="dropArrow"></div>
+                <ul class="languages dropdownWrapper">
+                    <li class="en alpha">
+                        <a href="https://www.pornhub.com/" data-lang="en" data-root="pornhub.com"
+                            onclick="ga('send', 'event', 'Language Flags Footer', 'click', 'English');">
+                            <span>English</span>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </section>
+    <div class="logoFooterWrapper homePageFooter">
+        <div class="socialIconsWrapper">
+            <a href="https://twitter.com/pornhub" rel="noopener" target="_blank"
+                title="Connectez-vous avec PornHub sur twitter ">
+                <img loading="lazy" class="lazy" id="twitterImage"
+                    src="data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7"
+                    alt="Logo de PH Twitter"
+                    data-src="https://di.phncdn.com/www-static/images/socialIcons/Twitter.svg?cache=2020040901"
+                    data-img_type="image">
+            </a>
+        </div>
+        <span class="copyright">© Pornhub.com, 2020</span>
+        <a href="/information#rating" rel="nofollow">
+            <div class="rta">
+                <img id="RTAImage" alt="RTA image"
+                    src="https://cdn1-smallimg.phncdn.com/n172nWs1UEcnquuObA5x52osw51230gH/rta-1.gif">
+            </div>
+        </a>
+    </div>
---
+++ resources/views/frontend/layout/partials/navbar.blade.php
@@ -0,0 +1,235 @@
+<header itemscope="" itemtype="http://schema.org/WPHeader" id="header">
+    <div id="headerWrapper">
+        <div id="headerContainer">
+            <div class="logo pornhub_logo ">
+                <div itemscope="" itemtype="http://schema.org/Organization" class="logoWrapper">
+                    <a itemprop="url" href="/" onclick="ga('send', 'event', 'Header', 'click', 'Logo');">
+                        <img itemprop="logo" title="Pornhub" alt="Pornhub Porn Videos" width="150" height="32"
+                            src="https://di.phncdn.com/www-static/images/pornhub_logo_straight.png?cache=2020040901">
+                    </a>
+                </div>
+            </div>
+            <div id="headerSearchWrapperFree" class="headerSearchWrapper">
+                <form autocomplete="off" id="search_form" action="/video/search" method="get">
+                    <fieldset class="fs-nf">
+                        <div id="searchBarContainer" class="searchBorder">
+                            <label for="search">
+                                <input id="searchInput" type="text" value=""
+                                    placeholder="Rechercher parmis les 11,624,689 vidéos..." maxlength="100"
+                                    name="search" aria-label="Search" accesskey="?"
+                                    data-token="MTU4NjU4ODEwNij4jXlFquGEYlUxtCBQxrwlHxZXiX_Pt-HaT6mui5WGXu7lQfWRsqu0rqBIHWyPwcqlBij6R0bXkB6A8jCiNQo."
+                                    data-orientation="straight" autocomplete="off">
+                            </label>
+                            <ul id="searchSuggestions"></ul>
+                            <div id="searchTypeWrapper" class="js_dropdownBound" data-dropdown-id="0">
+                                <div id="searchTypeSelected" class="dropdownTrigger" style="position: relative"
+                                    data-search-type="video">
+                                    <i class="video"></i>
+                                    <div id="js-toggleArrow" class="dropArrow"></div>
+                                    <div class="fakeClick"
+                                        style="position: absolute; top: 0; bottom: 0; right: 0; left: 0"></div>
+                                </div>
+                                <ul id="searchTypes" class="dropdownWrapper" style="display: none;">
+                                    <li class="alpha"><span>Que cherchez-vous&nbsp;?</span></li>
+                                    <li class="video" data-search-type="Vidéo"><i
+                                            class="video"></i><span>Vidéo</span></li>
+                                    <li class="photos" data-search-type="Photos"><i
+                                            class="photos"></i><span>Photos</span></li>
+                                    <li class="members" data-search-type="Membres"><i
+                                            class="members"></i><span>Membres</span></li>
+                                    <li class="pornstars" data-search-type="Pornstars"><i
+                                            class="pornstars"></i><span>Pornstars</span></li>
+                                    <li class="gifs" data-search-type="GIF"><i class="gifs"></i><span>GIF</span>
+                                    </li>
+                                    <li class="cam models omega" data-search-type="Camgirls"><i
+                                            class="cam models"></i><span>Camgirls</span></li>
+                                </ul>
+                            </div>
+
+                            <div id="btnSearch" class="orangeButton"><i></i></div>
+                        </div>
+                    </fieldset>
+                </form>
+            </div>
+
+
+
+            <span class="headerBtnsWrapper">
+                <a id="headerUploadBtn" class="greyButton"
+                    href="javascript:signinbox.show(textPHTranslation.loginTitleUploadVideo);"
+                    onclick="ga('send', 'event', 'Header', 'click', 'Upload');"><i></i>Importer</a>
+
+                <a id="headerUpgradePremiumBtn" class="orangeButton filterBtn removeAdLink"
+                    data-entrycode="UpgrBtn-Hdr_Star"
+                    onclick="triggerGatewayModal(event);ga('send', 'event', 'Header', 'click', 'upgrade');"
+                    style="background:#ffa31a" data-segment="straight" rel="nofollow"><i></i>Premium</a>
+            </span>
+
+            <div id="topRightProfileMenu" class="">
+
+                <a class="removeAdLink" id="headerLoginLink"
+                    href="/login?redirect=Fy3L39JbyTxaWYj8FHNhqD-mMYk9j3Q2jQgTGjUyzfY"
+                    onclick="signinbox.show(); ga('send', 'event', 'Header', 'click', 'Login'); return false;">Se
+                    connecter</a>
+                <a id="headerSignupLink" href="/create_account_select"
+                    onclick="ga('send', 'event', 'Header', 'click', 'Sign Up');">Inscrivez-vous !</a>
+            </div>
+        </div>
+    </div>
+
+    <div itemscope="" itemtype="http://www.schema.org/SiteNavigationElement" id="headerMenuContainer" class="">
+        <div id="headerMainMenuInner">
+            <div id="headerCampaignDiv">
+
+
+                <ul id="headerMainMenu" class="">
+                    <li itemprop="name" id="menuItem1" class="menu item-1 home smallerTxtSize alpha"
+                        data-hover="0">
+                        <a itemprop="url" href="/" rel="" class="active js-topMenuLink" target="_self"
+                            onclick="ga('send', 'event', 'Header', 'click', 'Home Tab');">
+                            <span class="itemName">Accueil<span class="activeLine"></span></span>
+                        </a>
+                    </li>
+                    <li itemprop="name" id="menuItem2" class="menu item-2 videos smallerTxtSize" data-hover="0">
+                        <a itemprop="url" href="/video" rel="" class=" js-topMenuLink" target="_self"
+                            onclick="ga('send', 'event', 'Header', 'click', 'Videos Tab');">
+                            <span class="itemName"><span class="arrowMenu">Vidéos</span><span
+                                    class="arrow arrowMenu"></span><span class="activeLine"></span></span>
+                        </a>
+                        <div class="wideDropdown videos js-dropdown " data-submenu-type="videos"
+                            style="display: none;">
+                            <div class="innerDropdown clearfix js-submenu">
+                                <div class="leftPanel videos">
+                                    <a href="/video" class="title">Découvrez les Vidéos</a>
+                                    <ul class="discover" style="display: none;">
+                                        <li class="alpha">
+                                            <a class="js-mixpanel" data-mixpanel-listing="" href="/recommended"
+                                                onclick="ga('send', 'event', 'Header', 'click', 'Recommended');">
+                                                <i class="discoverRecommended"></i>
+                                                Recommandé pour vous </a>
+                                        </li>
+                                    </ul>
+                        </div>
+                    </li>
+                    <li itemprop="name" id="menuItem3" class="menu item-3 categories smallerTxtSize"
+                        data-hover="0">
+                        <a itemprop="url" href="/categories" rel="" class=" js-topMenuLink" target="_self"
+                            onclick="ga('send', 'event', 'Header', 'click', 'Categories Tab');">
+                            <span class="itemName"><span class="arrowMenu">Catégories</span><span
+                                    class="arrow arrowMenu"></span><span class="activeLine"></span></span>
+                        </a>
+                        <div class="wideDropdown categories js-dropdown" data-submenu-type="categories"
+                            style="display: none;">
+                            <div class="innerDropdown clearfix js-submenu">
+                                <div class="leftPanel">
+                                    <a href="/categories" class="title">Découvrez les catégories</a>
+                                    <ul class="discover" style="display: none;">
+                                        <li class="alpha">
+                                            <a class="js-mixpanel" data-mixpanel-listing="" href="/categories">
+                                                <i class="discoverPopular"></i>
+                                                Populaire </a>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                    </li>
+                    <li itemprop="name" id="menuItem4" class="asyncLoad menu item-4 livesex smallerTxtSize"
+                        data-hover="0">
+                        <a itemprop="url" href="/live?track=6001" rel="" class=" js-topMenuLink" target="_self"
+                            onclick="ga('send', 'event', 'Header', 'click', 'Live Sex Tab');">
+                            <span class="itemName"><span class="arrowMenu">Webcams</span><span
+                                    class="arrow arrowMenu"></span><span class="activeLine"></span></span>
+                        </a>
+                        <div class="wideDropdown livesex js-dropdown" data-submenu-type="livesex">
+                            <div class="innerDropdown clearfix js-submenu">
+                            </div>
+                        </div>
+                    </li>
+                    <li itemprop="name" id="menuItem5" class="menu item-5 pornstar smallerTxtSize"
+                        data-hover="0">
+                        <a itemprop="url" href="/pornstars" rel="" class=" js-topMenuLink" target="_self"
+                            onclick="ga('send', 'event', 'Header', 'click', 'Porn Stars Tab');">
+                            <span class="itemName"><span class="arrowMenu">Pornstars</span><span
+                                    class="arrow arrowMenu"></span><span class="activeLine"></span></span>
+                        </a>
+                        <div class="wideDropdown pornstar js-dropdown" data-submenu-type="pornstar">
+                            <div class="innerDropdown clearfix js-submenu">
+                                <div class="leftPanel leftPanelLang">
+                                    <a href="/pornstars" class="title">Découvrez les Pornstars</a>
+                                    <ul class="discover">
+                                        <li class="alpha">
+                                            <a href="/pornstars">
+                                                <i class="discoverPopular"></i>
+                                                Les Plus Populaire </a>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                    </li>
+                    <li id="menuItem6" class="menu item-6 realsex smallerTxtSize" data-hover="0">
+                        <a href="https://p.purple6401.com/spot/20637.html" rel="nofollow noopener"
+                            class=" js-topMenuLink" target="_blank"
+                            onclick="ga('send', 'event', 'Header', 'click', 'Real Sex Tab');">
+                            <span class="itemName">FUCK NOW</span>
+                        </a>
+                    </li>
+                    <li itemprop="name" id="menuItem7" class="menu item-7 community smallerTxtSize"
+                        data-hover="0">
+                        <a itemprop="url" href="/community" rel="" class=" js-topMenuLink" target="_self"
+                            onclick="ga('send', 'event', 'Header', 'click', 'Community Tab');">
+                            <span class="itemName"><span class="arrowMenu">Communauté</span><span
+                                    class="arrow arrowMenu"></span><span class="activeLine"></span></span>
+                        </a>
+                        <div class="wideDropdown community js-dropdown " data-submenu-type="community">
+                            <div class="innerDropdown clearfix js-submenu">
+
+                                <div class="leftPanel">
+                                    <a href="/community" class="title">Découvrir la communauté</a>
+                                    <ul class="discover">
+                                        <li class="alpha">
+                                            <a href="/community">
+                                                <i class="discoverCommunityFeed"></i>
+                                                Flux de la communauté </a>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                    </li>
+                    <li itemprop="name" id="menuItem8" class="asyncLoad menu item-8 photos smallerTxtSize omega"
+                        data-hover="0">
+                        <a itemprop="url" href="/albums" rel="" class=" js-topMenuLink" target="_self"
+                            onclick="ga('send', 'event', 'Header', 'click', 'Photos Tab');">
+                            <span class="itemName"><span class="arrowMenu">Photos &amp; GIF</span><span
+                                    class="arrow arrowMenu"></span><span class="activeLine"></span></span>
+                        </a>
+                        <div class="wideDropdown photos js-dropdown" data-submenu-type="photos">
+                            <div class="innerDropdown clearfix js-submenu">
+                            </div>
+                        </div>
+                    </li>
+                </ul>
+
+                <div role="submenus-items-holder" style="display:none">
+
+                </div>
+
+                <script>
+                    var MENU_MAIN_HEADER = {
+                        "communityUrl": "\/front\/menu_community?segment=straight&token=MTU4NjU4ODEwNij4jXlFquGEYlUxtCBQxrwlHxZXiX_Pt-HaT6mui5WGXu7lQfWRsqu0rqBIHWyPwcqlBij6R0bXkB6A8jCiNQo.",
+                        "liveSexUrl": "\/front\/menu_livesex?segment=straight&token=MTU4NjU4ODEwNij4jXlFquGEYlUxtCBQxrwlHxZXiX_Pt-HaT6mui5WGXu7lQfWRsqu0rqBIHWyPwcqlBij6R0bXkB6A8jCiNQo.",
+                        "photosUrl": "\/front\/menu_photos?segment=straight&token=MTU4NjU4ODEwNij4jXlFquGEYlUxtCBQxrwlHxZXiX_Pt-HaT6mui5WGXu7lQfWRsqu0rqBIHWyPwcqlBij6R0bXkB6A8jCiNQo.",
+                        "preloadImage": "https:\/\/di.phncdn.com\/www-static\/images\/ajax-loader-small.gif?cache=2020040901",
+                        "menuUrl": "\/front\/menu_all_cached?segment=straight&token=MTU4NjU4ODEwNij4jXlFquGEYlUxtCBQxrwlHxZXiX_Pt-HaT6mui5WGXu7lQfWRsqu0rqBIHWyPwcqlBij6R0bXkB6A8jCiNQo."
+                    };
+
+                </script>
+            </div>
+        </div>
+
+
+    </div>
+
+</header>
